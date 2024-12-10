f11462ba3d72a9afff9d0a05e6880345d5a5f3a8
Matthew Elliott <matthew@seoworks.co.uk>
2024-12-10 15:37:27 +0000

    Added response when guess isnt between 1 and 20, better focus for player, player can press key enter instead of click

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 const message = document.querySelector('.message');
+const checkButton = document.querySelector('.check');
+
+document.querySelector('.guess').focus();
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    checkButton.click();
+  }
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // When there is no guess
   if (!guess) {
-    message.textContent = 'Please enter a number into the box!';
+    message.textContent =
+      'Please enter a number between 1 and 20 into the box!';
 
     //When guess is correct
   } else if (guess === secretNumber) {
@@ -26,7 +36,9 @@ document.querySelector('.check').addEventListener('click', function () {
     }
     //When guess is wrong
   } else if (guess !== secretNumber) {
-    if (score > 1) {
+    if (guess > 20 || guess < 1) {
+      message.textContent = 'Please use a number between 1 and 20';
+    } else if (score > 1) {
       message.textContent =
         guess > secretNumber
           ? 'Your guess is too high!'
@@ -50,7 +62,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').focus();
 });
 
-console.log('Hello');
 //High score
